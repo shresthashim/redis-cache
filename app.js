0b9de26e1113f59e521bd3d25bdb620647866448
AsHim Shrestha <ashimsth89@gmail.com>
2024-11-21 09:30:36 +0545

    feat: add Redis caching and performance benchmarking

--- app.js
+++ app.js
@@ -1,95 +1,90 @@
-import express from "express";
-import mongoose from "mongoose";
-import fs from "fs";
+import express from 'express';
+import mongoose from 'mongoose';
+import { createClient } from 'redis';
 
-// Initialize Express app
 const app = express();
 
-// MongoDB connection URI
-const mongoURI = "mongodb://root:root@localhost:27017/node_cache?authSource=admin";
+app.use(express.json());
 
-// Connect to MongoDB
-mongoose
-  .connect(mongoURI)
-  .then(() => {
-    console.log("Connected to MongoDB");
+const client = await createClient()
+    .on('error', (err) => console.log('Redis Client Error', err))
+    .connect();
 
-    // Automatically import data when the server starts, but only if no data exists
-    importData();
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
+mongoose.connect('mongodb://root:root@localhost:27017/node_cache?authSource=admin');
 
-// Define your product schema
-const productionSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  category: String,
-  specs: Object,
+const productSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+    category: String,
+    specs: Object,
 });
 
-// Create the product model
-const Product = mongoose.model("Product", productionSchema);
+const Product = mongoose.model('Product', productSchema);
 
-// Function to import data from data.json
-const importData = async () => {
-  try {
-    // Check if products already exist in the database
-    const existingProducts = await Product.countDocuments();
+app.get('/api/products', async (req, res) => {
+    const key = generateCacheKey(req);
 
-    if (existingProducts > 0) {
-      console.log("Data already exists in the database. Skipping import.");
-      return; // Skip import if data already exists
-    }
-
-    // Read the data from the JSON file
-    fs.readFile("data.json", "utf8", (err, data) => {
-      if (err) {
-        console.error("Error reading JSON file:", err);
+    const cachedProducts = await client.get(key);
+    if (cachedProducts) {
+        console.log('Cache hit');
+        res.json(JSON.parse(cachedProducts));
         return;
-      }
+    }
+    console.log('Cache miss');
 
-      // Parse the JSON data
-      const jsonData = JSON.parse(data);
-
-      // Insert the data into the MongoDB collection
-      Product.insertMany(jsonData)
-        .then(() => {
-          console.log("Data successfully inserted into MongoDB");
-        })
-        .catch((err) => {
-          console.error("Error inserting data:", err);
-        });
-    });
-  } catch (err) {
-    console.error("Error checking existing data:", err);
-  }
-};
-
-app.get("/api/products", async (req, res) => {
-  try {
     const query = {};
-
-    // Check if a category query parameter exists
-
     if (req.query.category) {
-      query.category = req.query.category;
+        query.category = req.query.category;
     }
 
-    // Get the products from the database
     const products = await Product.find(query);
 
-    // Return the products as JSON
+    if (products.length) {
+        await client.set(key, JSON.stringify(products));
+    }
+
     res.json(products);
-  } catch (err) {
-    console.error("Error getting products:", err);
-    res.status(500).send("An error occurred getting products");
-  }
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+function generateCacheKey(req) {
+    const baseUrl = req.path.replace(/^\/+|\/+$/g, '').replace(/\//g, ':');
+    const params = req.query;
+    const sortedParams = Object.keys(params)
+        .sort()
+        .map((key) => `${key}=${params[key]}`)
+        .join('&');
+
+    return sortedParams ? `${baseUrl}:${sortedParams}` : baseUrl;
+}
+
+app.put('/api/products/:id', async (req, res) => {
+    const productId = req.params.id;
+    const updateData = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+        productId,
+        { $set: updateData },
+        { new: true }
+    );
+
+    if (!updatedProduct) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found',
+        });
+    }
+
+    const listCacheKey = 'api:products*';
+    const keys = await client.keys(listCacheKey);
+    if (keys.length > 0) {
+        await client.del(keys);
+    }
+
+    res.json({
+        success: true,
+        message: 'Product updated successfully',
+    });
 });
+
+app.listen(4000, () => console.log('Server listening on port 4000'));
